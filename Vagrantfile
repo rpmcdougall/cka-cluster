70b0cf9dad22c5c4e223e7d9ab5b5e2a3ba1ed90
Ryan McDougall <rp.mcdougall@gmail.com>
2021-11-02 19:44:55 -0500

    add gitignore

--- Vagrantfile
+++ Vagrantfile
@@ -2,16 +2,24 @@ Vagrant.configure("2") do |config|
 
   config.vm.define "master" do |master|
     master.vm.box = "ubuntu/focal64"
-    master.vm.cpus = 2
-    master.vm.memory = 8192
+
+    master.vm.provider "virtualbox" do |v| 
+      v.memory = 8192
+      v.cpus = 2
+    end
     master.vm.network "private_network", ip: "10.1.1.5"
+    master.vm.provision "shell", path: "bootstrap.sh"
   end
 
   config.vm.define "worker" do |worker| 
     worker.vm.box = "ubuntu/focal64"
-    worker.vm.cpus = 2
-    worker.vm.memory = 8192
+    worker.vm.provider "virtualbox" do |v| 
+      v.memory = 8192
+      v.cpus = 2
+    end
+
     worker.vm.network "private_network", ip: "10.1.1.51"
+    worker.vm.provision "shell", path: "bootstrap.sh"
   end
 
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -8,29 +8,23 @@ apt get update && apt-get upgrade
 modprobe overlay
 modprobe br_netfilter
 
-cat > /etc/systcl.d/99-kubernetes-cri.conf << EOF
+cat << EOF > /etc/systcl.d/99-kubernetes-cri.conf 
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
 
 echo \
-  "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VER/$OS/ /" \
-  | tee -a /etc/apt/sources.list.d/cri-0.list
+"deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VER/$OS/ /" \
+| tee -a /etc/apt/sources.list.d/cri-0.list
 
-curl -L \
-  http://download.opensuse.org/repositories/devel:/kubiuc:/libcontainers:/stable:/cri-o:/$VER/$OS/Release.key \
-  | apt-key add -
+curl -L http://download.opensuse.org/repositories/devel:/kubiuc:/libcontainers:/stable:/cri-o:/$VER/$OS/Release.key | apt-key add -
 
 
-echo \ 
-  "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" \
-  | tee -a /etc/apt/sources.list.d/libcontainers.list
-
-
-curl -L \
-  https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key \
-  | apt-key add - 
+echo \
+"deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" \
+| tee -a /etc/apt/sources.list.d/libcontainers.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add - 
 
 apt-get update
 
@@ -42,7 +36,8 @@ systemctl enable crio
 systemctl start crio
 
 #K8s Install
-cat > /etc/apt/sources.list.d/kubernetes.list << EOF
+
+cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
